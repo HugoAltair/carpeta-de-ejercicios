d9c6f9a7e6a6e72abf4fbcd4c0dcbbbfb1553008
HugoAltair <hukranya@gmail.com>
2023-10-09 14:44:06 -0700

    session3 ejercicio 3 xdd

--- session3.empty.js
+++ session3.empty.js
@@ -2,27 +2,67 @@
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
 
-function addAll(numbers) {}
-
+function addAll() {
+  sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+function addAll2(numbers) {
+  let initialValue = 0;
+  let sum = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+  return sum;
+}
+console.log(addAll(2, 5, 6, 7, 8));
+//console.log(addAll2(2, 5, 6, 7, 8));
 // CHALLENGE 2: SUM ALL PRIMES
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
-function sumAllPrimes(limit) {}
+function sumAllPrimes(limit) {
+  let sumPrimes = 0,
+    primes = [];
+  for (i = 1; i <= limit; i++) primes.push(i); //transform the limit into an array
+  primes.forEach((el) => {
+    let acum = 0;
+    for (j = 1; j <= limit; j++) {
+      if (el % j === 0) acum += 1;
+    }
+    if (acum === 2) {
+      // a prime number is a whole number greater than 1 whose only factors are 1 and itself
+      sumPrimes += el;
+    }
+  });
+  return sumPrimes;
+}
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(15));
-// console.log(sumAllPrimes(21));
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(15));
+console.log(sumAllPrimes(21));
 
 // CHALLENGE 3: SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-function seekAndDestroy(input, ...params) {}
+function seekAndDestroy(input, ...params) {
+  let newInput = input.join(" ");
+  console.log(typeof newInput);
+  for (i = 0; i < params.length; i++) {
+    //newInput.replace(new RegExp(JSON.stringify(params[i]), "ig"), "");
+    let newParams = params[i].toString();
+    newInput.replace(new RegExp(newParams, "ig"), "");
+    console.log(newParams);
+  }
+  return newInput;
+}
 
-// console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6));
+console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6));
 // console.log(seekAndDestroy([1, 2, 2, 6, 6, "hello"], 2, 6));
-// console.log(seekAndDestroy([9, 8, 7, 5, 5, "hello"], 5));
+//console.log(seekAndDestroy([9, 8, 7, 5, 5, "hello"], 5));
 
 // CHALLENGE 4: SORT BY HEIGHT
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
@@ -57,3 +97,9 @@ function evenOddSums(arr) {}
 
 // const output = evenOddSums([50, 60, 60, 45, 71]);
 // console.log(output);
+const eliminarPatron = (str = "", patron = "") => {
+  if (!str) return console.warn("No ingresaste texto");
+  if (!patron) return console.warn("no ingresaste el patron");
+  console.info(str.replace(new RegExp(patron, "ig"), "")); //la bandera i es para que no distinga de mayusculas y minusculas, la g para una busqueda global
+};
+//eliminarPatron("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
